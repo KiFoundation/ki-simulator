3d3787041b2529a08c74d1c03c60750dc29936e6
Tarek Awwad <TarekAwwad@users.noreply.github.com>
2019-02-14 14:08:47 +0100

    Prediction models added

--- transactionGenerator.py
+++ transactionGenerator.py
@@ -1,16 +1,21 @@
 import sys
 import pandas as pd
 import numpy as np
+import itertools
 from datetime import timedelta
 from datetime import datetime
 import matplotlib.pyplot as plt
 import seaborn as sns
+import statsmodels.api as sm
 
+from pandas.plotting import register_matplotlib_converters
+
+register_matplotlib_converters()
 np.random.seed = 1
 
 # Data generator config
 start_date = '2/11/2018'
-number_of_days = 730
+number_of_days = 3000
 end_date = datetime.strptime(start_date, "%d/%m/%Y") + timedelta(days=number_of_days)
 time_per_block = 600
 sampling_freq = 'D'
@@ -51,11 +56,13 @@ thresholds = {1: 0.5, 2: -10, 3: -10}
 inflation = 100000
 base_reward = 5.5
 
+data_file = 'data/bitcoin/n-transactions-all.csv'
+# data_file = 'data/ethereum/export-TxGrowth.csv'
 
 def generate_blocks():
     # Trend from data
     if trend == 'data':
-        df = pd.read_csv('data/n-transactions-all.csv', names=['date', 'data'], nrows=number_of_days, skiprows=700)
+        df = pd.read_csv(data_file, names=['date', 'data'], nrows=number_of_days, skiprows=0)
         df['data'] /= tx_ampl_param
 
     else:
@@ -83,6 +90,8 @@ def generate_blocks():
         # Round and scale the values
         df = df.round({'data': 3})
         df['data'] *= tx_ampl_param
+
+    forecasting(df)
 
     # Split the time units to block : 1 block every 8 seconds
     df_blocks = pd.DataFrame([0 for i in range(len(df) * block_per_time_unit)], columns=['tx'])
@@ -228,16 +237,16 @@ def compute_reward_hybrid(df_blocks_, static_split_):
             #             len(df_blocks_) * 2)
             dynamic_reward = 2
 
-        else :
-            print((len(df_blocks_rewards) - index) * max([np.mean(df_blocks_['tx'][:index]), 1]), sum(df_blocks_['tx'][index:]))
+        else:
+            # print((len(df_blocks_rewards) - index) * max([np.mean(df_blocks_['tx'][index-1:index]), 1]), sum(df_blocks_['tx'][index:]))
 
             dynamic_reward = epochs[row['epoch']] * (row['tx'] * (dc - payed_dynamic_rewards)) / (
-                        (len(df_blocks_rewards) - index) * max([np.mean(df_blocks_['tx'][:index]), 1]))
+                    (len(df_blocks_rewards) - index) * max([np.mean(df_blocks_['tx'][:index]), 1]))
 
         static_reward = (1 - epochs[row['epoch']]) * sc / len(df_blocks_)
         payed_dynamic_rewards = payed_dynamic_rewards + dynamic_reward
 
-        r_hat =  static_reward  + dynamic_reward
+        r_hat = static_reward + dynamic_reward
         df_blocks_rewards.iloc[index] = [row['tx'], r_hat, row['epoch']]
 
     print(sum(df_blocks_rewards['reward']))
@@ -250,7 +259,7 @@ def compute_reward_hybrid(df_blocks_, static_split_):
     round_ = 51
     for i in range(int(len(df_blocks_rewards) / round_) + 1):
         mean_reward_per_round = np.mean(df_blocks_rewards['reward'][i * round_:i * round_ + round_])
-        mean_tx_per_round.append( np.mean(df_blocks_rewards['tx'][i * round_:i * round_ + round_]))
+        mean_tx_per_round.append(np.mean(df_blocks_rewards['tx'][i * round_:i * round_ + round_]))
         mean_rewards_per_round.append(mean_reward_per_round)
         mean_rewards_per_round_x.append(i)
         # mean_rewards_per_round_epoch.append(df_blocks_rewards.iloc[i * round_]['epoch'])
@@ -274,7 +283,88 @@ def distribute_validators(dict_of_validators):
     return
 
 
+def forecasting(df_blocks_):
+    df_blocks_['date'] = pd.to_datetime(df_blocks_['date'])
+    df_blocks_ = df_blocks_.set_index('date')
+    df_blocks_ = df_blocks_['data'].resample('MS').mean()
+    print(len(df_blocks_))
+
+    decomposition = sm.tsa.seasonal_decompose(df_blocks_, model='multiplicative')
+    fig = decomposition.plot()
+
+    arima(df_blocks_, False)
+
+
+def parameter_selection(df_blocks_):
+    p = d = q = range(0, 2)
+    pdq = list(itertools.product(p, d, q))
+    seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
+
+    ress = []
+
+    for param in pdq:
+        for param_seasonal in seasonal_pdq:
+            try:
+                mod = sm.tsa.statespace.SARIMAX(df_blocks_,
+                                                order=param,
+                                                seasonal_order=param_seasonal,
+                                                enforce_stationarity=False,
+                                                enforce_invertibility=False)
+                results = mod.fit()
+                ress.append([param, param_seasonal, results.aic])
+                # print('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
+            except:
+                continue
+
+    return np.asarray(sorted(ress, key=lambda x: x[2], reverse=True))[-1]
+
+
+def arima(df_blocks_, param_select):
+    # Select parameters
+    a = parameter_selection(df_blocks_)
+
+    mod = sm.tsa.statespace.SARIMAX(df_blocks_, order=a[0], seasonal_order=a[1],
+                                    enforce_stationarity=False, enforce_invertibility=False)
+    # Fit the selection model
+    results = mod.fit()
+
+    # Plot diagnostics
+    results.plot_diagnostics(figsize=(16, 8))
+
+    # Get and plot predictions staring a given date
+    pred = results.get_prediction(start=pd.to_datetime('2015-01-01'), dynamic=False)
+    pred_ci = pred.conf_int()
+
+    df_ = pd.DataFrame({'date': df_blocks_['2014':].index.values,
+                        'data': [df_blocks_['2014':][i] for i in df_blocks_['2014':].index.values]})
+
+    ax = df_.set_index('date').plot(label='observed')
+
+    pred.predicted_mean.plot(ax=ax, label='One-step ahead Forecast', alpha=.7, figsize=(14, 7))
+    ax.fill_between(pred_ci.index, pred_ci.iloc[:, 0], pred_ci.iloc[:, 1], color='k', alpha=.2)
+    ax.set_xlabel('Date')
+    plt.legend()
+
+    # Compute MSE and RMSE error
+    y_forecasted = pred.predicted_mean
+    y_truth = df_blocks_['2017-01-01':]
+    mse = ((y_forecasted - y_truth) ** 2).mean()
+    print('The Root Mean Squared Error of our forecasts is {}'.format(round(np.sqrt(mse), 2)))
+
+    pred_uc = results.get_forecast(steps=100)
+    pred_ci = pred_uc.conf_int()
+    ax = df_blocks_.plot(label='observed', figsize=(14, 7))
+    pred_uc.predicted_mean.plot(ax=ax, label='Forecast')
+    ax.fill_between(pred_ci.index,
+                    pred_ci.iloc[:, 0],
+                    pred_ci.iloc[:, 1], color='k', alpha=.25)
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Furniture Sales')
+    plt.legend()
+
+    plt.show()
+
 res = generate_blocks()
 # compute_reward_block(res)
 # compute_reward_transaction(res)
-compute_reward_hybrid(res, 0.1)
+# compute_reward_hybrid(res, 0.1)
