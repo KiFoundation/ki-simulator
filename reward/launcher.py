e87bd86d1f4ce5862371e33734163c6d3e26e810
Tarek Awwad <TarekAwwad@users.noreply.github.com>
2019-02-25 13:15:36 +0100

    BU commit

--- reward/launcher.py
+++ reward/launcher.py
@@ -3,9 +3,10 @@ from kisimulator.reward.rewarder import *
 from kisimulator.reward.predictor import *
 
 res = generate_data()
-# print(res)
-compute_reward_transfer(res, 0.2)
 
+# print(res)
+# compute_reward_transfer(res, 0.2)
+# generate_validators(300, 'normal')
 
 # res = generate_transactions_per_time_unit()
 # forecasting(res)
